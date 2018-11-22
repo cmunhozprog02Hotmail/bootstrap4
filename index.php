da9c338c7784325df6012a782c7b5b181f9bda86
Christovam Munhoz <cmunhozprog02@hotmail.com>
2018-11-22 00:54:31 -0200

    grid

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css"">
+</head>
+<body>
+    <div class="container">
+        <a class="btn btn-primary mt-5" href="instalacaolocal.html">Introdução Local</a>
+        <a class="btn btn-info mt-5" href="grid.html">Sistema de Grid</a>
+
+    </div>
+    
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
